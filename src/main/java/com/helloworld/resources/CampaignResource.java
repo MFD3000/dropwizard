fdf36a1ea70a309a2be94c838551433c5ffa6854
dcapri <dan.capri@segmint.com>
2013-01-29 19:51:37 -0500

    updating to current

--- src/main/java/com/helloworld/resources/CampaignResource.java
+++ src/main/java/com/helloworld/resources/CampaignResource.java
@@ -1,13 +1,10 @@
 package com.helloworld.resources;
 
-
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import com.helloworld.jdbi.CampaignDAO;
 
 import java.util.List;
